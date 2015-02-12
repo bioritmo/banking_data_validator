6f283dc365f2c4c5e0b4cf0953212339ced36e6d
Americo Duarte <americodls@gmail.com>
2015-02-12 17:27:21 -0200

    Add new validator: HSBC

--- lib/banking_data_validator/bank/hsbc.rb
+++ lib/banking_data_validator/bank/hsbc.rb
@@ -1,8 +1,25 @@
-require "banking_data_validator/bank/common"
+require "banking_data_validator/bank/base"
 
 module BankingDataValidator
   module Bank
-    class HSBC < Common
+    class HSBC < Base
+      # private
+
+      def checksum
+        "#{raw_checksum % 10}"
+      end
+
+      def raw_checksum
+        @raw_checksum ||= (multiply_factors % 11)
+      end
+
+      def factors
+        @factors ||= (2..9).reverse_each.cycle
+      end
+
+      def digits
+        "#{@branch}#{@account_number}".chars.map(&:to_i)
+      end
     end
   end
 end
---
+++ spec/banking_data_validator/bank/hsbc_spec.rb
@@ -0,0 +1,23 @@
+require "banking_data_validator/bank/hsbc"
+
+module BankingDataValidator
+  module Bank
+    RSpec.describe HSBC do
+      describe "#valid_account?" do
+        it "returns true when is given an account_number with a valid digit" do
+          expect(HSBC.valid_account?("0007", "853838", "6")).to eq(true)
+          expect(HSBC.valid_account?("1823", "023467", "7")).to eq(true)
+          expect(HSBC.valid_account?("0336", "006625", "3")).to eq(true)
+          expect(HSBC.valid_account?("0885", "008468", "2")).to eq(true)
+        end
+
+        it "returns false when is given an account_number with an invalid digit" do
+          expect(HSBC.valid_account?("0007", "853838", "9")).to eq(false)
+          expect(HSBC.valid_account?("1823", "023467", "9")).to eq(false)
+          expect(HSBC.valid_account?("0336", "006625", "9")).to eq(false)
+          expect(HSBC.valid_account?("0885", "008468", "9")).to eq(false)
+        end
+      end
+    end
+  end
+end
